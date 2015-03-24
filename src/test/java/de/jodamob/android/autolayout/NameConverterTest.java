ce960cec9367ada7dd718a43551b0607d0b29db7
Danny Preussler <dpreussler@groupon.com>
2015-03-24 09:19:54 +0100

    fixed test compilation

--- src/test/java/de/jodamob/android/autolayout/NameConverterTest.java
+++ src/test/java/de/jodamob/android/autolayout/NameConverterTest.java
@@ -5,7 +5,6 @@ import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 
 import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
 
 @RunWith(RobolectricTestRunner.class)
 public class NameConverterTest {
